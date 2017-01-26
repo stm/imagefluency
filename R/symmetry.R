1f19d7add127f512cbbc2f860ab19ab324abc897
stm <news.mayer@gmail.com>
2017-01-26 11:07:06 +0100

    symmetry function: fixed error (wrong image dimensions); added documentation

--- R/symmetry.R
+++ R/symmetry.R
@@ -1,5 +1,32 @@
 # 2do: what if image width / height cannot be divided by 2?
+# 2do: what if sd is zero (no corr possible because no variation due to the fact that there is no variation in one half of the image?)
 
+
+#' quantify_symmetry
+#'
+#' quantify_symmetry returns the vertical and horizontal
+#' symmetry of an image. Values can range between 0 (not
+#' symmmetrical) and 1 (fully symmetrical).
+#'
+#' @param img A matrix of numeric values or integer values.
+#'   Color images have to be converted to grayscale in
+#'   advance or each color channel has to be analyzed
+#'   seperately.
+#'
+#' @return a list of numeric values
+#' @export
+#'
+#' @examples
+#' # construct sample image
+#' img <- matrix(0, nrow=100, ncol=100)
+#' img[21:60,31:70] <- 0.5
+#' img[71:90, 21:80] <- 1
+#'
+#' # if you want to inspect the image
+#' # OpenImageR::imageShow(img)
+#'
+#' # get both vertical and horizontal symmetry
+#' quantify_symmetry(img)
 quantify_symmetry <- function(img){
   sym_v <- sym_ver(img)
   sym_h <- sym_hor(img)
@@ -7,14 +34,28 @@ quantify_symmetry <- function(img){
     return(results)
 }
 
+
+#' sym_ver
+#'
+#' sym_ver returns the vertical symmetry of a grayscale
+#' image matrix. To this end, the left and right image
+#' halves are correlated.
+#'
+#' @param img A matrix of numeric values or integer values.
+#'
+#' @return a numeric value between 0 and 1
 sym_ver <- function(img){
   ## -----------------------
   ##   vertical symmetry
   ## -----------------------
 
-  # cut image into 2 equal pieces (horizontally, that means across the x axis)
-  stimL <- img[ , 1:(stimW/2)]
-  stimR <- img[ , (1 + stimW/2):stimW ]
+  # image dimensions
+  imgH <- dim(img)[1] # image height
+  imgW <- dim(img)[2] # image width
+
+  # cut image into 2 equal pieces (row-wise / vertically, that means across the y axis)
+  stimL <- img[ , 1:(imgW/2)]
+  stimR <- img[ , (1 + imgW/2):imgW ]
 
   # flip right image half
   stimRfl <- OpenImageR::flipImage(stimR, mode = "horizontal")
@@ -27,17 +68,30 @@ sym_ver <- function(img){
   corrLR <- cor(pixL,pixR)
 
   # final symmetry: vertical (absolute correlation)
-  sym_v <- abs(corrLR)
+  return(abs(corrLR))
 }
 
+#' sym_hor
+#'
+#' sym_hor returns the horizontal symmetry of a grayscale
+#' image matrix. To this end, the upper and lower image
+#' halves are correlated.
+#'
+#' @param img A matrix of numeric values or integer values.
+#'
+#' @return a numeric value between 0 and 1
 sym_hor <- function(img){
   ## -----------------------
   ##   horizontal symmetry
   ## -----------------------
 
-  # cut image into 2 equal pieces (vertically, that means across the y axis)
-  stimU <- img[ 1:(stimH/2), ]
-  stimD <- img[ (1 + stimH/2):stimH, ]
+  # image dimensions
+  imgH <- dim(img)[1] # image height
+  imgW <- dim(img)[2] # image width
+
+  # cut image into 2 equal pieces (column-wise / horizontally, that means across the x axis)
+  stimU <- img[ 1:(imgH/2), ]
+  stimD <- img[ (1 + imgH/2):imgH, ]
 
   # flip lower image half
   stimDfl <- OpenImageR::flipImage(stimD, mode = "vertical")
@@ -50,7 +104,6 @@ sym_hor <- function(img){
   corrUD <- cor(pixU,pixD)
 
   # final symmetry: horizontal (absolute correlation)
-  sym_h <- abs(corrUD)
-
+  return(abs(corrUD))
 }
 
