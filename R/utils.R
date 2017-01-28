99cec54b15ab3419cd4e47e1e9a691a2d5a4918b
stm <news.mayer@gmail.com>
2017-01-28 14:31:34 +0100

    utils.R: make it more robust

--- R/utils.R
+++ R/utils.R
@@ -8,7 +8,8 @@
 #' @return An error message if the check fails.
 #' @keywords internal
 .check_input <- function(img, f_call = NULL){
-  if (f_call == "symmetry" | f_call == "contrast" | f_call == "typicality" | f_call == "self-similarity") {
+  if (is.null(f_call)) stop("You have to specify function for the f_call argument.", call. = FALSE)
+  if (f_call == "symmetry" | f_call == "contrast" | f_call == "typicality" | f_call == "complexity" | f_call == "self-similarity") {
     # input must be a matrix of numeric or integer values
     if (!is.matrix(img)) {
       stop("Input has to be a *matrix* of numeric or integer values", call. = FALSE)
@@ -16,5 +17,5 @@
     if (!(is.numeric(img) | is.integer(img))) {
       stop("Input has to be a matrix of *numeric* or *integer* values", call. = FALSE)
     }
-  }
+  } else stop("unknown input to f_call argument", call. = FALSE)
 }
