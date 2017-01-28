a22ef18dd349dd03122cb3ac37661ba1bf431e4d
stm <news.mayer@gmail.com>
2017-01-28 14:33:37 +0100

    typicality: added documentation

--- R/typicality.R
+++ R/typicality.R
@@ -1,12 +1,62 @@
-## -----------------------
-##      typicality
-## -----------------------
-# visual typicality is the correlation of a particular image with the average
-# representation, i.e. the mean of all images
-#
-# NOTE: The function takes a list of grayscale images as argument.
 
+#' Typicality of Images relative to each other
+#'
+#' \code{quantify_typicality} returns the visual typicality
+#' of a list of image matrices \code{imglist} relative to
+#' each other. Higher values indicate larger typicality. The
+#' typicality score is computed as the correlation of a
+#' particular image with the average representation of all
+#' images, i.e. the mean of all images.\cr\cr Rescaling of
+#' the images prior to computing the typicality scores can
+#' be specified with the optional rescaling parameter.
+#'
+#'
+#' @param imglist A list of matrices with numeric values or
+#'   integer values. Color images have to be converted to
+#'   grayscale in advance or each color channel has to be
+#'   analyzed seperately.
+#' @param rescale numeric. Rescales the images prior to
+#'   computing the typicality scores (per default no
+#'   rescaling is performed). Rescaling is performed by the
+#'   \code{resizeImage} function from the \code{OpenImageR}
+#'   package (bilinear rescaling)
+#'
+#' @return a list with a matrix of numeric value (typicality
+#'   scores)
+#' @export
+#'
+#' @examples
+#' # construct a list of 20 sample images
+#' imgs <- replicate(20,
+#'   matrix(runif(100, min = 0, max = 255), nrow = 10, ncol = 10),
+#'   simplify = FALSE)
+#'
+#' # get typicality scores
+#' quantify_typicality(imgs)
+#'
+#' @references Mayer, S. & Landwehr, J. R. (2016). Measuring
+#'   design typicality -- a comparison of objective and
+#'   subjective approaches. \emph{Proceedings of DRS 2016,
+#'   Design Research Society 50th Anniversary Conference}.
+#'   Brighton, UK, 27–30 June 2016.
+#'
+#' @importFrom stats cor
 quantify_typicality <- function(imglist, rescale = 1){
+
+  # check input
+  if (!is.list(imglist)) {
+    stop("Input has to be a *list* of image matrices", call. = FALSE)
+  }
+  #tryCatch(lapply(imglist, .check_input, "typicality"), error = function(err) {stop("errorinside", call. = FALSE)})
+  for (elmnt in seq_along(imglist)) {
+    if (!is.matrix( imglist[[elmnt]] )) {
+      stop(paste("List element",elmnt,"has to be a *matrix* of numeric or integer values"))
+    }
+    if (!(is.numeric( imglist[[elmnt]] ) | is.integer( imglist[[elmnt]] ))) {
+      stop(paste("List element",elmnt,"has to be a matrix of *numeric* or *integer* values"))
+    }
+  }
+
   # original resolution or different scaling level?
   if (rescale != 1) {
     # image dimensions of first element (assumes the same for all elements)
@@ -20,6 +70,8 @@ quantify_typicality <- function(imglist, rescale = 1){
   # create matrix of vectorized intensity values
   imglist <- matrix(unlist(imglist), ncol = length(imglist), byrow = FALSE)
   img_mean <- rowMeans(imglist)
-  return(list(typicality = cor(imglist, img_mean)))
+  output <- cor(imglist, img_mean)
+  rownames(output) <- paste0("img", 1:nrow(output))
+  return(list(typicality = output))
 }
 
