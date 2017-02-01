349d41a77698b976eb2da753f92af65b5e76ffc7
stm <news.mayer@gmail.com>
2017-02-01 19:18:44 +0100

    typicality.R: rescale per default NULL, check if parameter is numeric; warning if too few images in list

--- R/typicality.R
+++ R/typicality.R
@@ -54,11 +54,15 @@
 #'   \code{\link{quantify_contrast}},
 #'   \code{\link{quantify_self_similarity}}
 #' @importFrom stats cor
-quantify_typicality <- function(imglist, rescale = 1){
+quantify_typicality <- function(imglist, rescale = NULL){
 
   # check input
   if (!is.list(imglist)) {
     stop("Input has to be a *list* of image matrices", call. = FALSE)
+  }
+  if (length(imglist) < 2) {
+    warning("The function needs at least 2 images in the input list. Returning NA.", call. = FALSE)
+    return(list(typicality = NA))
   }
   #tryCatch(lapply(imglist, .check_input, "typicality"), error = function(err) {stop("errorinside", call. = FALSE)})
   for (elmnt in seq_along(imglist)) {
@@ -66,26 +70,28 @@ quantify_typicality <- function(imglist, rescale = 1){
       stop(paste("List element",elmnt,"has to be a *matrix* of numeric or integer values"))
     }
     if (!(is.numeric( imglist[[elmnt]] ) | is.integer( imglist[[elmnt]] ))) {
-      stop(paste("List element",elmnt,"has to be a matrix of *numeric* or *integer* values"))
+      stop(paste("List element",elmnt,"has to be a matrix of *numeric* or *integer* values"), call. = FALSE)
     }
   }
 
   # original resolution or different scaling level?
-  if (rescale != 1) {
+  if (!is.null(rescale)) {
     if (!is.numeric(rescale)) {
-      stop("parameter 'rescale' must be numeric")
+      stop("parameter 'rescale' must be numeric", call. = FALSE)
     }
-    # Rescaling requires package "OpenImageR"
-    if (requireNamespace("OpenImageR", quietly = TRUE)) {
-      # image dimensions of first element (assumes the same for all elements)
-      img_h <- dim(imglist[[1]])[1] # image height
-      img_w <- dim(imglist[[1]])[2] # image width
-      imglist <- lapply(imglist, OpenImageR::resizeImage,
-                        width = rescale*img_w,
-                        height = rescale*img_h,
-                        method = "bilinear")
-    } else {
-      stop("Package 'OpenImageR' is required for rescaling but not installed on your system.")
+    if (rescale != 1) {
+      # Rescaling requires package "OpenImageR"
+      if (requireNamespace("OpenImageR", quietly = TRUE)) {
+        # image dimensions of first element (assumes the same for all elements)
+        img_h <- dim(imglist[[1]])[1] # image height
+        img_w <- dim(imglist[[1]])[2] # image width
+        imglist <- lapply(imglist, OpenImageR::resizeImage,
+                          width = rescale*img_w,
+                          height = rescale*img_h,
+                          method = "bilinear")
+      } else {
+        stop("Package 'OpenImageR' is required for rescaling but not installed on your system.", call. = FALSE)
+      }
     }
   }
   # create matrix of vectorized intensity values
