7f0c55372f29df58fbb5f8c27deee956eb325d69
stm <news.mayer@gmail.com>
2017-02-02 19:02:07 +0100

    complete rewrite of complexity function based on `magick` package

--- R/complexity.R
+++ R/complexity.R
@@ -18,44 +18,48 @@ NULL
 #'
 #' \code{quantify_complexity} returns the complexity of an
 #' image as the size of the zip-compressed image file.
-#' Supported images types are \code{.bmp}, \code{.png},
-#' \code{.jpg}, and \code{.jpeg}.
 #'
 #' @details The function returns the visual complexity of an
 #'   image. Visual complexity is calculated as the size of
-#'   the zip-compressed image file. Preferably, the input
-#'   is an uncompressed image file. If the input image is not an
-#'   uncompressed \code{.bmp} file, the function tries to
+#'   the zip-compressed image file. Preferably, the input is
+#'   an uncompressed image file. If the input image is not
+#'   an uncompressed \code{.bmp} file, the function tries to
 #'   create an uncompressed version of the image.
 #'
-#'   The function takes the file path of an image file or a pre-loaded image as input argument (\code{imgfile}) and returns
-#'   the filesize of the zip-compressed image file.
+#'   The function takes the file path of an image file (or URL) or a
+#'   pre-loaded image as input argument (\code{imgfile}) and
+#'   returns the filesize of the zip-compressed image file.
 #'   Additionally, the uncompressed filesize and the
 #'   compression rate are returned. Note that permission to
-#'   read and write files in the specified directory path / working directory is
-#'   needed for this function to work properly.
+#'   read and write files in the specified directory path /
+#'   working directory is needed for this function to work
+#'   properly.
 #'
 #'   The zip compression algorithm works line by line and
 #'   hence does not depict horizontal and vertical
-#'   redundancies equally (see Example 1). Thus, the function includes an
-#'   optional \code{rotate} parameter (default:
+#'   redundancies equally (see Example). Thus, the function
+#'   includes an optional \code{rotate} parameter (default:
 #'   \code{FALSE}). Setting this parameter to \code{TRUE}
-#'   has the following effects: first, the image is loaded
-#'   via the \code{readbitmap} package (if \code{imgfile} is
-#'   not a pre-loaded image). Second, the image is
-#'   rotated by 90 degrees. Third, the rotated image is
-#'   written in the same directory as the input file (or the working directory) and a
-#'   zip-compressed version is created. Finally, the
-#'   complexity of the image is copmuted as the minimum of
-#'   the original image's filesize and the filesize of the
-#'   rotated image. All files that are created while the
-#'   function runs are automatically deleted once the
-#'   complexity score is calculated.
+#'   has the following effects: first, the image is rotated
+#'   by 90 degrees. Second, the rotated image is written in
+#'   the working directory and a zip-compressed version is
+#'   created. Finally, the complexity of the image is
+#'   computed as the minimum of the original image's
+#'   filesize and the filesize of the rotated image. All
+#'   files that are created while the function runs are
+#'   automatically deleted once the complexity score is
+#'   calculated.
 #'
-#'   In the current version, only \code{.bmp}, \code{.png},
-#'   \code{.jpg}, and \code{.jpeg} images are supported.
+#'   As \code{R}'s built-in \code{bmp} device creates (a)
+#'   indexed instead of True Color images and (b) creates
+#'   files with different file sizes depending on the
+#'   operating system, the function relies on the
+#'   \code{magick} package to write (and read) images.
 #'
-#' @param imgfile A character string containing the file name of the image or an array or a matrix of numeric or integer values representing the pre-loaded image.
+#' @param imgfile Either a character string containing the
+#'   file name of the image (or URL) or an 3-dimensional
+#'   array of numeric values representing the pre-loaded
+#'   image.
 #' @param rotate logical. Should the compressed filesize of
 #'   the rotated image also be computed? (see details)
 #'
@@ -65,40 +69,23 @@ NULL
 #' @export
 #'
 #' @examples
-#' # Example 1: pre-loaded image as input
-#' #
-#' # create sample image
-#' img <- matrix(0, nrow = 500, ncol = 500)
-#' img[, 201:300] <- 1
+#' # Example: URL as input
+#' # sample image is a Lenna (http://www.lenna.org)
+#' # an uncompressed version can be found at http://www.hlevkin.com/TestImages/classic.htm
+#' quantify_complexity("http://www.hlevkin.com/TestImages/lenna.bmp")
+#' quantify_complexity("http://www.hlevkin.com/TestImages/lenna.bmp", rotate = TRUE)
 #'
-#' # get complexity
-#' quantify_complexity(img)
-#' quantify_complexity(img, rotate = TRUE) # rotated file smaller (i.e., zip works line by line)
+#' # example image with high complexity
+#' # http://www.hlevkin.com/TestImages/barbara.bmp
 #'
-#'
-#' # Example 2: image path as input
-#' #
-#' # Download Lena sample image (see http://eeweb.poly.edu/~yao/EL5123/SampleData.html)
-#' download.file("http://eeweb.poly.edu/~yao/EL5123/image/lena_gray.bmp",
-#'    "Lena.bmp", mode = "wb")
-#'
-#' # get complexity
-#' quantify_complexity("Lena.bmp")
-#'
-#' # get complexity (including rotated version)
-#' quantify_complexity("Lena.bmp", rotate = TRUE)
-#'
-#' # compute simplicity score
-#' lena_results <- quantify_complexity("Lena.bmp", rotate = TRUE)
-#' lena_results$original / lena_results$compressed # simplicity ratio
-#'
-#' # delete image of Lena
-#' file.remove("Lena.bmp")
+#' # example image with low complexity
+#' # http://www.hlevkin.com/TestImages/baboon.bmp
 #'
 #' @references Donderi, D. C. (2006). Visual complexity: A
 #'   Review. \emph{Psychological Bulletin}, \emph{132},
 #'   73--97.
 #'   doi:\href{https://doi.org/10.1037/0033-2909.132.1.73}{10.1037/0033-2909.132.1.73}
+#'
 #'
 #' @references Forsythe, A., Nadal, M., Sheehy, N.,
 #'   Cela-Conde, C. J., & Sawey, M. (2011). Predicting
@@ -107,175 +94,121 @@ NULL
 #'   49--70.
 #'   doi:\href{https://doi.org/10.1348/000712610X498958}{10.1348/000712610X498958}
 #'
+#'
 #' @seealso \code{\link{quantify_symmetry}},
 #'   \code{\link{quantify_contrast}},
 #'   \code{\link{quantify_typicality}},
 #'   \code{\link{quantify_self_similarity}}
 #'
-#' @importFrom grDevices bmp dev.off
-#' @importFrom graphics plot
 #' @importFrom utils zip
 quantify_complexity <- function(imgfile, rotate = FALSE){
 
-  if (is.character(imgfile) & !is.matrix(imgfile) & !is.array(imgfile)) {
+  if (is.character(imgfile) & !is.array(imgfile)) {
     if (length(imgfile) == 1) {
       input <- "image_path"
     } else {
       stop("Multiple filenames. Function can only handle one image at a time.", call. = FALSE)
     }
-  } else if ((is.matrix(imgfile) || is.array(imgfile)) & (is.numeric(imgfile) | is.integer(imgfile))) {
-    if (is.array(imgfile) & !is.matrix(imgfile) & (length(dim(imgfile)) != 3 | dim(imgfile)[3] != 3)) {
+  } else if (is.array(imgfile) & (is.numeric(imgfile) | is.integer(imgfile))) {
+    if (is.array(imgfile) & (length(dim(imgfile)) != 3 | dim(imgfile)[3] != 3)) {
       stop("Wrong array dimensions (has to be a 3-dimensional array)", call. = FALSE)
     } else {
       input <- "image"
     }
   } else {
-    stop("Wrong type of input: has to be a filename (character string) or an image (array or matrix of numeric or integer values)", call. = FALSE)
+    stop("Wrong type of input: has to be a filename (character string) or an image (3-dimensional array of numeric or integer values)", call. = FALSE)
   }
 
+  if (!requireNamespace("magick", quietly = TRUE)) stop("Package 'magick' not found but needed. Please install the package first.", call. = FALSE)
 
-  if (input == "image_path") {
+  # actual function
+  quant_compl <- function(imgfile, rotate) {
+    flname <- .rand_string()
+    img <- magick::image_read(imgfile)
+    imginfo <- magick::image_info(img)
 
-    try_err_file = inherits(tryCatch(normalizePath(imgfile, mustWork = TRUE), error = function(err) err), "error")
-    if (!try_err_file) {
-      imgfile <- normalizePath(imgfile)
+    isBMP <- grepl("^BMP", as.character(imginfo[1]))
+    if (!isBMP) warning("Input image might not be uncompressed. Interpret results with caution.", call. = FALSE)
+
+    # write bmp image
+    magick::image_write(img, path = paste0(flname, ".bmp"), format = "bmp")
+
+    # zip-compress file
+    Sys.sleep(0.25) # wait a moment
+    suppressWarnings(zip(zipfile = flname, files = paste0(flname, ".bmp")))
+
+    # read file sizes
+    orig_size <- file.size(paste0(flname, ".bmp"))
+    compressed_size <- file.size(paste0(flname,".zip"))
+
+    # remove newly created files
+    Sys.sleep(0.25) # wait a moment
+    file.remove(paste0(flname, ".bmp"))
+    file.remove(paste0(flname, ".zip"))
+
+    if (!is.logical(rotate)) {
+      warning(paste0("rotate = '", rotate, "' is not a logical value (TRUE/FALSE). Skipping rotation ..."), call. = FALSE)
+      rotate <- FALSE
+    }
+
+    if (rotate) {
+      flname_rot <- paste0(flname, "_rot")
+
+      message("... trying to rotate the image ...")
+      # write rotated image
+      magick::image_write(magick::image_rotate(img, degrees = 90), path = paste0(flname_rot, ".bmp"), format = "bmp")
+
+      # zip-compress rotated image and read file size
+      Sys.sleep(0.25) # wait a moment
+      zip(zipfile = flname_rot, files = paste0(flname_rot, ".bmp"))
+      compressed_size_rot <- file.size(paste0(flname_rot,".zip"))
+
+      # remove rotated files
+      Sys.sleep(0.25) # wait a moment
+      file.remove(paste0(flname_rot,".zip"))
+      file.remove(paste0(flname_rot, ".bmp"))
+
+      # update result: minimum of both compressed file sizes of original and rotated img
+      compressed_size <- min(compressed_size, compressed_size_rot)
+    }
+
+    # return results
+    return(list(compressed = compressed_size, original = orig_size, ratio = compressed_size/orig_size))
+  }
+
+  try_err_file <- inherits(tryCatch(magick::image_read(imgfile), error = function(err) err), "error")
+  if (try_err_file) {
+    if (input == "image_path") {
+      stop(paste0("File not found or invalid path (could not resolve input)"), call. = FALSE)
     } else {
       stop(paste0("File not found or invalid path (could not resolve '", imgfile ,"')"), call. = FALSE)
     }
-
-    # original file name without file extension
-    # #file_wo_ext <- gsub("(.*)\\.bmp$", "\\1", imgfile)
-    flext <- sub(".*\\.([^.]+)$", "\\1", imgfile)
-    if (!(tolower(flext) %in% c("bmp", "png", "jpeg", "jpg"))) {
-      stop(paste0("Unsupported filename extension .", flext,
-                  ". Currently only the following image types are supported: .bmp, .png, .jpg, .jpeg"), call. = FALSE)
-    }
-    file_wo_ext <- gsub(paste0("(.*)\\.",flext,"$"), "\\1", imgfile)
-
-    # new filename with random string to make sure nothing is overwritten
-    flname_uncompressed <- paste0(file_wo_ext, "_", .rand_string())
-  } else if (input == "image") {
-    flext <- "none"
-    flname_uncompressed <- .rand_string()
-  }
-
-
-  if (tolower(flext) != "bmp") {
-
-    if (input != "image") {
-      warning("Input image is not an uncompressed bitmap. Conversion to .bmp was performed. Results might not be meaningful.", call. = FALSE)
-      # flnme is character (checked above)
-      if (requireNamespace("readbitmap", quietly = TRUE)) {
-        img <- readbitmap::read.bitmap(imgfile)
-      } else {
-        stop("Package 'readbitmap' not found but needed load the image.")
-      }
-    } else {
-      img <- imgfile
-    }
-
-    # image dimensions
-    img_h <- dim(img)[1] # image height
-    img_w <- dim(img)[2] # image width
-
-    # write bmp image
-    bmp(filename = paste0(flname_uncompressed, ".bmp"), width = img_h, height = img_w)
-    if (requireNamespace("OpenImageR", quietly = TRUE)) {
-      OpenImageR::imageShow(img)
-    } else if (requireNamespace("grid", quietly = TRUE)) {
-      if (max(img) > 1) {
-        img <- img / 255
-      }
-      grid::grid.raster(img)
-    } else {
-      stop("Package 'OpenImageR' or 'grid' needed to render the uncompressed image.")
-    }
-    dev.off()
-
-    # original file size
-    orig_size <- file.size(paste0(flname_uncompressed, ".bmp"))
-
-    # zip-compress file
-    Sys.sleep(0.5) # wait for half a second
-    suppressWarnings(zip(zipfile = flname_uncompressed, files = paste0(flname_uncompressed, ".bmp")))
-
-    # remove newly created .bmp file
-    Sys.sleep(0.5) # wait for half a second
-    file.remove(paste0(flname_uncompressed, ".bmp"))
-
   } else {
-    # original file size
-    orig_size <- file.size(imgfile)
-
-    # zip-compress file
-    Sys.sleep(0.5) # wait for half a second
-    suppressWarnings(zip(zipfile = flname_uncompressed, files = imgfile))
+    quant_compl(imgfile, rotate)
   }
-
-  # read file size of compressed file
-  compressed_size <- file.size(paste0(flname_uncompressed,".zip"))
-  # remove newly created .zip file
-  Sys.sleep(0.5) # wait for half a second
-  file.remove(paste0(flname_uncompressed,".zip"))
-
-  if (!is.logical(rotate)) {
-    warning(paste0("rotate = '", rotate, "' is not a logical value (TRUE/FALSE). Skipping rotation ..."), call. = FALSE)
-    rotate <- FALSE
-  }
-
-  if (rotate) {
-
-    message("... trying to rotate the image ...")
-
-    # no img as parameter given but image path, try to load original image
-    if (input == "image_path") {
-      # flnme is character (checked above)
-      if (requireNamespace("readbitmap", quietly = TRUE)) {
-        img <- readbitmap::read.bitmap(imgfile)
-      } else {
-        stop("Package 'readbitmap' not found but needed to load the .bmp image for rotation")
-      }
-    }
-
-    # image dimensions
-    img_h <- dim(img)[1] # image height
-    img_w <- dim(img)[2] # image width
-
-    # rotate image
-    if (requireNamespace("OpenImageR", quietly = TRUE)) {
-      img_rot <- OpenImageR::rotateFixed(img,90)
-    } else {
-      img_rot <- rotate90(img)
-    }
-    # write as uncompressed image file
-    flname_rot <- paste0(flname_uncompressed, "_rot")
-    bmp(filename = paste0(flname_rot, ".bmp"), width = img_w, height = img_h) # height and width switched because of rotation
-    if (requireNamespace("OpenImageR", quietly = TRUE)) {
-      OpenImageR::imageShow(img_rot)
-    } else if (requireNamespace("grid", quietly = TRUE)) {
-      if (max(img_rot) > 1) {
-        img_rot <- img_rot / 255
-      }
-      grid::grid.raster(img_rot)
-    } else {
-      stop("Package 'OpenImageR' or 'grid' needed to render the rotated image.")
-    }
-    dev.off()
-
-    # zip-compress rotated image and read file size
-    Sys.sleep(0.5) # wait for half a second
-    zip(zipfile = flname_rot, files = paste0(flname_rot, ".bmp"))
-    compressed_size_rot <- file.size(paste0(flname_rot,".zip"))
-    # remove rotated files
-    Sys.sleep(0.5) # wait for half a second
-    file.remove(paste0(flname_rot,".zip"))
-    file.remove(paste0(flname_rot, ".bmp"))
-
-    # update result: minimum of both compressed file sizes of original and rotated img
-    compressed_size <- min(compressed_size, compressed_size_rot)
-  }
-
-  # return results
-  return(list(compressed = compressed_size, original = orig_size, ratio = compressed_size/orig_size))
 }
 
+
+# # not in use yet
+# quantify_entropy <- function(img) {
+#
+#   binMin <- 0
+#   binMax <- 1
+#   if (max(img) > 1) binMax <- 255
+#
+#   # calculate histogram (with 256 bins as in Matlab)
+#   imgHist <- hist(as.vector(img), breaks = seq(binMin, binMax, length.out = 256), plot = FALSE)$density
+#   #imgHist <- hist(img, breaks = seq(binMin, binMax, length.out = 256), plot = FALSE)$counts
+#
+#   # remove zero entries
+#   imgHist[imgHist==0] <- NA
+#   imgHist <- na.omit(imgHist)
+#
+#   # normalize
+#   imgNorm <- imgHist
+#   #imgNorm <- imgHist / sum(imgHist)
+#
+#   # entropy
+#   entr <- -sum(imgNorm * log2(imgNorm))
+#   return(entr)
+# }
--- tests/testthat/test_complexity.R
+++ tests/testthat/test_complexity.R
@@ -4,27 +4,48 @@ test_that("quantify_complexity handles only *numeric* matrices or arrays", {
   expect_error(quantify_complexity(array(1, dim = c(10, 10, 2))),
                "Wrong array dimensions \\(has to be a 3-dimensional array\\)")
   expect_error(quantify_complexity(matrix("foo", nrow = 10, ncol = 10)),
-               "Wrong type of input\\: has to be a filename \\(character string\\) or an image \\(array or matrix of numeric or integer values\\)")
+               "Wrong type of input\\: has to be a filename \\(character string\\) or an image \\(3-dimensional array of numeric or integer values\\)")
   expect_error(quantify_complexity(1:10),
-               "Wrong type of input\\: has to be a filename \\(character string\\) or an image \\(array or matrix of numeric or integer values\\)")
+               "Wrong type of input\\: has to be a filename \\(character string\\) or an image \\(3-dimensional array of numeric or integer values\\)")
 })
 
 test_that("quantify_complexity warns if rotate is not a logical value", {
-  expect_warning(quantify_complexity(matrix(1, 100, 100), rotate = "yes"),
+  expect_warning(quantify_complexity(array(0, dim = c(10, 10, 3)), rotate = "yes"),
                  "rotate = 'yes' is not a logical value \\(TRUE\\/FALSE\\)\\. Skipping rotation \\.\\.\\.")
 })
 
-# test_that("quantify_complexity works as intended", {
-#   set.seed(2787)
-#   img <- matrix(round(runif(200*200, min = 0, max = 255)), nrow = 100, ncol = 100)
-#   results <- quantify_complexity(img)
-#   expect_equal(results$original, 1198, tolerance = 100, scale = 1)
-#   expect_equal(results$compressed, 581, tolerance = 100, scale = 1)
-#   expect_equal(results$ratio, 0.484975, tolerance = 0.01, scale = 1)
-#
-#   img1 <- matrix(0, nrow = 100, ncol = 100)
-#   img1[c(1:10, 21:30, 41:50, 61:70, 81:90), ] <- 1
-#   img2 <- rotate90(img1, direction = "negative")
-#   expect_lte(quantify_complexity(img1)$compressed, quantify_complexity(img2)$compressed)
-#   expect_equal(quantify_complexity(img1)$compressed, quantify_complexity(img2, rotate = TRUE)$compressed, tolerance = 10, scale = 1)
-# })
+test_that("quantify_complexity warns if input is not a bmp image", {
+  expect_warning(quantify_complexity(array(0, dim = c(10, 10, 3))),
+                 "Input image might not be uncompressed\\. Interpret results with caution\\.")
+})
+
+
+test_that("quantify_complexity works as intended", {
+  set.seed(2787)
+  redChannel <- matrix(round(runif(250*250, min = 0, max = 1)), nrow = 250, ncol = 250)
+  greenChannel <- matrix(round(runif(250*250, min = 0, max = 1)), nrow = 250, ncol = 250)
+  blueChannel <- matrix(round(runif(250*250, min = 0, max = 1)), nrow = 250, ncol = 250)
+  img <- array(c(redChannel, greenChannel, blueChannel), dim = c(dim(redChannel), 3))
+  # magick::image_read(img)
+  expect_warning(results <- quantify_complexity(img),
+                 "Input image might not be uncompressed\\. Interpret results with caution\\.")
+
+  expect_equal(results$compressed, 28990, tolerance = 100, scale = 1)
+  expect_equal(results$original, 188138, tolerance = 100, scale = 1)
+  expect_equal(results$ratio, 0.154089, tolerance = 0.01, scale = 1)
+
+
+  redChannel <- matrix(0, nrow = 300, ncol = 300)
+  redChannel[101:300, ] <- 1
+  greenChannel <- matrix(0, nrow = 300, ncol = 300)
+  greenChannel[201:300, ] <- 1
+  blueChannel <- matrix(0, nrow = 300, ncol = 300)
+  img <- array(c(redChannel, greenChannel, blueChannel), dim = c(dim(redChannel), 3))
+  # magick::image_read(img)
+  expect_warning(results <- quantify_complexity(img),
+                 "Input image might not be uncompressed\\. Interpret results with caution\\.")
+
+  expect_equal(results$compressed, 552, tolerance = 10, scale = 1)
+  expect_equal(results$original, 270138, tolerance = 100, scale = 1)
+  expect_equal(results$ratio, 0.0020434, tolerance = 0.001, scale = 1)
+})
