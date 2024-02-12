5eb45ba67d1e3a2d36cc7a611c64f7e1f73ecabe
Stefan Mayer <stm@users.noreply.github.com>
2024-02-13 00:16:30 +0100

    fixes #9 (incorrect parameter name in internal function's documentation)

--- R/complexity.R
+++ R/complexity.R
@@ -135,7 +135,7 @@ img_complexity <- function(imgfile, algorithm = "zip", rotate = FALSE){
 #'
 #' Returns the complexity of an image array / matrix or path.
 #'
-#' @param imgfile An array or matrix of numeric values or integer values, or the file path to the image.
+#' @param img An array or matrix of numeric values or integer values, or the file path to the image.
 #' @param algorithm character. Which compression algorithm to use.
 #' @param rotate logical. Image rotation by 90 degrees?
 #'
